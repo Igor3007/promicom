3c16961b15ddbcfb2d8b814f3e7375c259bb4b87
DenOn1x <den.onix2002@gmail.com>
2023-04-10 14:42:29 +0300

    mod optimize files 2.0

--- dist/static/js/optimize.js
+++ dist/static/js/optimize.js
@@ -1,12 +1,15 @@
 //Load scripts after page load
 window.addEventListener("load", function () {
-    let splide = document.createElement("script");
+    var splide = document.createElement("script");
     splide.src = "/static/js/splide.min.js";
     splide.onload = function (){
-        new Splide( '.optimize-slider__items', {
+        new Splide( '.clients-block__slider', {
+            mediaQuery: 'min',
             arrows: true,
             pagination: false,
-
+            gap: 16,
+            perPage: 1,
+            drag: false
         }).mount();
 
     }
